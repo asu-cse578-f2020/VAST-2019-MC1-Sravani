2c0010b2c97756f7edaa51edd81d2c2897099312
Rithika Mamidi <rithikamamidi309@gmail.com>
2020-11-01 17:55:48 -0700

    mean and entropy api added

--- Backend/app.py
+++ Backend/app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 import pandas as pd
-from preprocessData import get_damage_mean_by_category
+from preprocessData import get_damage_mean_by_category, get_mean_by_category, get_entropy_by_category
 
 app = Flask(__name__)
 
@@ -16,12 +16,23 @@ def st_himark_geo_json():
 @app.route('/damage/mean/<string:category>', methods=['GET'])
 def damage_by_category(category):
     damage_mean_by_category = get_damage_mean_by_category(category)
-    print(damage_mean_by_category)
     damage = pd.DataFrame(damage_mean_by_category)
     damage = damage.to_json(orient='records')
-    print("------------------------------------------------------")
-    print(damage)
     return damage
+
+@app.route('/damage/mean/<string:category>/<string:time1>/<string:time2>', methods=['GET'])
+def damage_by_category_for_time_period(category, time1, time2):
+    damage_mean_by_category_for_timeframe = get_mean_by_category(category, time1, time2)
+    damage = pd.DataFrame(damage_mean_by_category_for_timeframe)
+    damage = damage.to_json(orient='records')
+    return damage
+
+@app.route('/damage/entropy/<string:category>/<string:time1>/<string:time2>', methods=['GET'])
+def entropy_by_category_for_time_period(category, time1, time2):
+    entropy_by_category_for_timeframe = get_entropy_by_category(category, time1, time2)
+    entropy = pd.DataFrame(entropy_by_category_for_timeframe)
+    entropy = entropy.to_json(orient='records')
+    return entropy
 
 if __name__ == '__main__':
     app.debug = True
--- Backend/preprocessData.py
+++ Backend/preprocessData.py
@@ -5,15 +5,17 @@ import numpy as np
 import time
 import datetime
 import functools
-nba=pd.read_csv("Data//mc1-reports-data.csv")
+import collections
+import math
+from scipy.stats import entropy
 
+nba=pd.read_csv("Data//mc1-reports-data.csv",parse_dates=['time'])
 
-
+nba=nba.set_index('time')
+nba['time']=nba.index
 
 def get_damage_mean_by_category(category):
     nba["time"]= pd.to_datetime(nba["time"])
-    print(nba["time"].max(),nba["time"].min())
-    print(nba["time"].max()-nba["time"].min())
     nba['hour'] = nba.time.dt.hour
     nba['date']=nba.time.dt.date
     nba_location=nba.groupby(nba.location,as_index=False)
@@ -23,7 +25,6 @@ def get_damage_mean_by_category(category):
     for group_name,location in nba_location:
         locations_daysData=0*[5]
         temp=nba_location.get_group(group_name).dropna(subset=[category])
-        #temp.to_excel(category+'_'+str(group_name)+'_withoutblanks.xlsx')
         temp=temp.set_index('time')
         temp['time']=temp.index
         locations_daysData.append(temp.loc['2020-04-06'])
@@ -37,15 +38,35 @@ def get_damage_mean_by_category(category):
     maxList = max(locations_timelyAverage, key = lambda i: len(i))
     maxLength = len(maxList)
     df = functools.reduce(lambda left,right: pd.merge(left,right,on='time',how='outer'),locations_timeAnalysis)
-    # df.to_excel(category+'.xlsx')
     damage = pd.DataFrame(df)
-    i=0
-    locations = ['Time', 'Palace Hills', 'Northwest', 'Old Town', 'Safe Town', 'Southwest', 'Downtown', 'Wilson Forest', 'Scenic Vista', 'Broadview', 'Chapparal', 'Terrapin Springs', 'Pepper Mill', 'Cheddarford', 'Easton', 'Weston', 'Southton', 'Oak Willow', 'East Parton', 'West Parton']
+    locations = ["time", "palace_hills", "northwest", "old_town", "safe_town", "southwest", "downtown", "wilson_forest", "scenic_vista", "broadview", "chapparal", "terrapin_springs", "pepper_mill", "cheddarford", "easton", "weston", "southton", "oak_willow", "east_parton", "west_parton"]
     damage.columns = locations
-    for col in damage.columns: 
-        i = i+1
-        print(col, i)
     return damage
-    
 
-# get_damage_mean_by_category("medical")
+def get_mean_by_category(category,t1,t2):
+    mask = (nba['time'] >= t1) & (nba['time'] <= t2)
+    temp = nba.loc[mask]
+    nba_location=temp.groupby(temp.location,as_index=False)
+    locations=0*[19]
+    locations_timelyAverage=0*[19]    
+    for group_name,location in nba_location:
+        temp_category=nba_location.get_group(group_name).dropna(subset=[category])
+        locations_timelyAverage.append([group_name,temp_category[category].mean()])
+    df=pd.DataFrame(locations_timelyAverage)
+    return df
+
+def get_entropy_by_category(category,t1,t2):
+    mask = (nba['time'] >= t1) & (nba['time'] <= t2)
+    temp = nba.loc[mask]
+    nba_location=temp.groupby(temp.location,as_index=False) 
+    locations=0*[19]
+    locations_timelyAverage=0*[19]
+    for group_name,location in nba_location:
+        temp_category=nba_location.get_group(group_name).dropna(subset=[category])
+        bases = collections.Counter([tmp_base for tmp_base in temp_category[category]])
+        dist = [x/sum(bases.values()) for x in bases.values()]
+        entropy_value = entropy(dist, base=11)
+        locations_timelyAverage.append([group_name,entropy_value]) 
+    
+    df=pd.DataFrame(locations_timelyAverage)
+    return df
