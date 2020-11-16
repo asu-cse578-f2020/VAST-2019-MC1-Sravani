9d1a033bf1e9d0f3285002a041ec37e56212e95d
Sravani sajjan <gsravani1906@gmail.com>
2020-11-16 00:18:36 -0700

    fixed CORS

--- Backend/app.py
+++ Backend/app.py
@@ -1,9 +1,10 @@
 from flask import Flask
 import pandas as pd
 from preprocessData import get_damage_mean_by_category, get_mean_by_category, get_entropy_by_category
+from flask_cors import CORS
 
 app = Flask(__name__)
-
+CORS(app)
 
 @app.route('/geo-json', methods=['GET'])
 def st_himark_geo_json():
