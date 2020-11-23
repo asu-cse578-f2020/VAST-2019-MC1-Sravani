c390648e10bd4e2da3df484a7ab2b5313845421c
Sravani sajjan <gsravani1906@gmail.com>
2020-11-22 17:11:00 -0700

    Map category selection changes

--- Frontend/scripts/map.js
+++ Frontend/scripts/map.js
@@ -14,29 +14,84 @@ document.addEventListener("DOMContentLoaded", function () {
   ]).then(function (values) {
     mapData = values[0];
     topoData = topojson.feature(mapData, mapData.objects.StHimark);
-    Promise.all([getMeanData(), getEntropyData()]).then(function () {
-      drawMap();
-    });
+    Promise.all([getMeanData(), getEntropyData(), getMapMeanData()]).then(
+      function () {
+        drawMap();
+      }
+    );
   });
 });
 
+async function getMapMeanData(
+  startDate = "2020-04-06%2000:35:00",
+  endDate = "2020-04-06%2001:35:00"
+) {
+  url =
+    "http://localhost:5000/damage/mean/allcategories/" +
+    startDate +
+    "/" +
+    endDate;
+  let newdata = await d3.json(
+    "http://localhost:5000/damage/mean/allcategories/'2020-04-06%2000:35:00'/'2020-04-06%2001:35:00'"
+  );
+  console.log("newdata", url, newdata);
+}
+
 // Draw the map in the #map svg
-function drawMap() {
-  console.log("powerMean", powerMean, powerEntropy);
+function drawMap(dimension) {
+  var colorScheme;
+  switch (dimension) {
+    case "power":
+      MapMeanArray = powerMean;
+      MapEntropyArray = powerEntropy;
+      colorScheme = d3.schemeReds[3];
+      break;
+    case "buildings":
+      MapMeanArray = buildingsMean;
+      MapEntropyArray = buildingsEntropy;
+      colorScheme = d3.schemeBlues[3];
+      break;
+    case "medical":
+      MapMeanArray = medicalMean;
+      MapEntropyArray = medicalEntropy;
+      colorScheme = d3.schemeGreens[3];
+      break;
+    case "shake":
+      MapMeanArray = shakeMean;
+      MapEntropyArray = shakeEntropy;
+      colorScheme = d3.schemePurples[3];
+      break;
+    case "sewer":
+      MapMeanArray = sewerMean;
+      MapEntropyArray = sewerEntropy;
+      colorScheme = d3.schemeOranges[3];
+      break;
+    case "roads":
+      MapMeanArray = roadsMean;
+      MapEntropyArray = roadsEntropy;
+      colorScheme = d3.schemePuRd[3];
+      break;
+    default:
+      MapMeanArray = powerMean;
+      MapEntropyArray = powerEntropy;
+      colorScheme = d3.schemeReds[3];
+      break;
+  }
+  console.log("dimension", dimension, MapMeanArray);
   topoData.features.forEach(function (d) {
     d.val = +Math.floor(Math.random() * (10 - 0 + 1)) + 0;
   });
   // create the map projection and geoPath
   let projection = d3
     .geoMercator()
-    .scale(100000)
+    .scale(129000)
     .center(d3.geoCentroid(topoData))
     .translate([
       +mapSvg.style("width").replace("px", "") / 2,
       +mapSvg.style("height").replace("px", "") / 2.3,
     ]);
   let path = d3.geoPath().projection(projection);
-  var colorScale = d3.scaleOrdinal(d3.schemeReds[3]).domain([0, 10]);
+  var colorScale = d3.scaleOrdinal(colorScheme).domain([0, 10]);
   let g = mapSvg.append("g");
   g.selectAll("path")
     .data(topoData.features)
@@ -48,15 +103,15 @@ function drawMap() {
     })
     .attr("fill", (d) => {
       //let val = +Math.floor(Math.random() * (10 - 0 + 1)) + 0;
-      let val = powerMean[d.properties.Id - 1];
+      let val = MapMeanArray[d.properties.Id - 1];
       //console.log(val, colorScale(val));
       return colorScale(val);
     })
     .attr("stroke", "black")
     .attr("stroke-width", (d) => {
-      entropyVal = powerMean[d.properties.Id - 1];
-      if (entropyVal >= 0 && entropyVal <= 3) return 0.5;
-      if (entropyVal >= 4 && entropyVal <= 7) return 2;
-      if (entropyVal >= 8 && entropyVal <= 10) return 4;
+      entropyVal = MapEntropyArray[d.properties.Id - 1];
+      if (entropyVal >= 0 && entropyVal <= 0.3) return 4;
+      if (entropyVal >= 0.4 && entropyVal <= 0.7) return 2;
+      if (entropyVal >= 0.8) return 0.5;
     });
 }
