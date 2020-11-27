7a0bc16e307ac4a3111157448ad5229f1f9edde7
sanjana1706 <Sanjanadasari3@gmail.com>
2020-11-26 18:12:22 -0700

    legends changed

--- Frontend/scripts/stackedBarChart.js
+++ Frontend/scripts/stackedBarChart.js
@@ -57,7 +57,7 @@ function drawStackedBarChart(data) {
     .domain(data.map(function a(d) {
       return d.total_country;
     }))
-    .range([0, 1.2 * height]);
+    .range([0, 1.3 * height]);
 
   var x = d3.scaleLinear()
     .domain([
@@ -66,44 +66,50 @@ function drawStackedBarChart(data) {
         return d.total;
       }),
     ])		// y = d3.scaleLinear()
-    .rangeRound([0, 1 * width]);	// .rangeRound([height, 0]);
+    .rangeRound([0, 1.45 * width]);	// .rangeRound([height, 0]);
   var z = d3
     .scaleOrdinal()
     .domain(function (data) { return data.columns.slice(0, 5) })
-    .range(["#44a2c2", "#eb4034", "#e0c71f", "#a2c7fa", "#4346f7", "#875133",]);
+    .range(["rgb(201,148,199)", "rgb(188,189,220)", "rgb(161,217,155)", "rgb(242,146,114)", "rgb(158,202,225)", "rgb(253,174,107)",]);
 
   svg.select('.yaxis').remove();
   svg.select('.xaxis').remove();
   svg.select('.ylabel').remove();
   svg.select('.xlabel').remove();
   // y-axis
-  svg.append("g")
-    .attr("transform", `translate(${margin.left},130)`)
+  const customYAxis =  svg.append("g")
+    .attr("transform", `translate(${margin.left},100)`)
     .attr("class", "yaxis")
-    .call(d3.axisLeft(y).tickSize(`10`));
+    .call(d3.axisLeft(y).tickSize(`5`));
 
   svg.append("text")
     .attr("class","ylabel")
     .attr("transform", "rotate(-90)")
     .attr("y", 0)
-    .attr("x", 0 - (height / 1))
+    .attr("x", 0 - (height / 1.2))
     .attr("dy", "1em")
     .style("text-anchor", "middle")
     .text("Countries");
 
+
   // x-axis
   svg.append("g")
-    .attr("transform", `translate(${margin.left},${1.582* height})`)
+    .attr("transform", `translate(${margin.left},${1.595* height})`)
     .attr("class", "xaxis")
     .call(d3.axisBottom(x).tickSize(`10`));
 
   svg.append("text")
   .attr("class","xlabel")
     .attr("transform",
-      "translate(" + (width / 1.5) + " ," +
-      (1.3*height + margin.top + 120) + ")")
+      "translate(" + (width/1.35) + " ," +
+      (1.2*height + margin.top + 150) + ")")
     .style("text-anchor", "middle")
     .text("Intensities");
+  
+    d3.selectAll(".yaxis>.tick>text")
+    .each(function(d, i){
+      d3.select(this).style("font-size",16);
+    });
 
   var tooltip = d3.select("body")
     .append("div")
@@ -113,43 +119,51 @@ function drawStackedBarChart(data) {
   let legend = svg.append('g')
     .attr("transform", `translate(${margin.left},${0})`)
 
-  legend.append("circle").attr("cx", 425).attr("cy", 175).attr("r", 6).style("fill", "#eb4034")
-  legend.append("text").attr("x", 450).attr("y", 178).text('shake_intensity')
+  legend.append("circle").attr("cx", 10).attr("cy", 25).attr("r", 6).style("fill", "rgb(188,189,220)")
+  legend.append("text").attr("x", 25).attr("y", 28).text('shake_intensity')
 
-  legend.append("circle").attr("cx", 425).attr("cy", 200).attr("r", 6).style("fill", "#e0c71f")
-  legend.append("text").attr("x", 450).attr("y", 203).text('medical')
+  legend.append("circle").attr("cx", 225).attr("cy", 25).attr("r", 6).style("fill", "rgb(161,217,155)")
+  legend.append("text").attr("x", 240).attr("y", 28).text('medical')
 
-  legend.append("circle").attr("cx", 425).attr("cy", 225).attr("r", 6).style("fill", "#a2c7fa")
-  legend.append("text").attr("x", 450).attr("y", 228).text('power')
+  legend.append("circle").attr("cx", 450).attr("cy", 25).attr("r", 6).style("fill", "#FA8072")
+  legend.append("text").attr("x", 465).attr("y", 28).text('power')
 
-  legend.append("circle").attr("cx", 425).attr("cy", 250).attr("r", 6).style("fill", "#4346f7")
-  legend.append("text").attr("x", 450).attr("y", 253).text('buildings')
+  legend.append("circle").attr("cx", 10).attr("cy", 60).attr("r", 6).style("fill", "rgb(158,202,225)")
+  legend.append("text").attr("x", 25).attr("y", 63).text('buildings')
 
-  legend.append("circle").attr("cx", 425).attr("cy", 275).attr("r", 6).style("fill", "#875133")
-  legend.append("text").attr("x", 450).attr("y", 278).text('sewer_and_water')
+  legend.append("circle").attr("cx", 225).attr("cy", 60).attr("r", 6).style("fill", "rgb(253,174,)")
+  legend.append("text").attr("x", 240).attr("y", 63).text('sewer_and_water')
 
-  legend.append("circle").attr("cx", 425).attr("cy", 300).attr("r", 6).style("fill", "#44a2c2")
-  legend.append("text").attr("x", 450).attr("y", 303).text('roads_and_bridges')
+  legend.append("circle").attr("cx", 450).attr("cy", 60).attr("r", 6).style("fill", "rgb(201,148,199)")
+  legend.append("text").attr("x", 465).attr("y", 63).text('roads_and_bridges')
 
   var series = d3.stack().keys(["shake_intensity", "medical", "power", "buildings", "sewer_and_water", "roads_and_bridges"])(data).map(d => (d.forEach(v => v.key = d.key), d));
-  svg.append("g")
+  var bar =  svg.append("g")
     .selectAll("g")
-    .data(series)
-    .enter().append("g")
+    .data(series);
+
+    bar
+    .transition()
+      .attr("x", function(d){ return x(d.data['total_country']); })
+      .attr("y", function(d){ return y(d[1]); })
+      .attr("height", function(d){ return x(d[1]) - x(d[0]); });
+
+    bar.enter().append("g")
     .attr("fill", function (d) { return z(d.key); })
     .selectAll("rect")
     .data(function (d) { return d; })
     .enter().append("rect")
     .attr("y", function (d) { return y(d.data.total_country); })	    //.attr("x", function(d) { return x(d.data.State); })
-    .attr("x", function (d) { return x(d[0]); })			    //.attr("y", function(d) { return y(d[1]); })	
+    .attr("x", function (d) { return x(d[0]) })			    //.attr("y", function(d) { return y(d[1]); })	
     .attr("width", function (d) { return x(d[1]) - x(d[0]); })	//.attr("height", function(d) { return y(d[0]) - y(d[1]); })
     .attr("height", 40)
-    .attr("transform", `translate(${margin.left},${margin.top + 115})`)
+    .attr("transform", `translate(${margin.left},${margin.top + 85})`)
     .style("cursor", "pointer")
     .on('mouseover', function (d, i) {
       tooltip.style('class', '.tooltip')
         .text(function () {
-          return "value: " + (`${(d[1] - d[0]).toFixed(2)}`);
+          console.log(d)
+          return "value: " + (`${(d[1] - d[0]).toFixed(2)}`)+"\nTotal Intensity: "+ (`${(d.data.total).toFixed(2)}`);
         })
         .style('opacity', 1)
         .style("left", (d3.event.pageX + 10) + "px")
